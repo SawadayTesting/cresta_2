87050dd0c1be2ad0ab7a77d7ef764623cc6147f6
SawadaToshihito <>
2020-04-22 13:04:38 +0900

    Javascript / 終了
    TODO / Swiper, マージ

--- product/js/header.js
+++ product/js/header.js
@@ -1,23 +1,20 @@
-//headerの動き
-//jQuery使用
-
-$(function () {
+//スティッキーヘッダー
+$(function(){
 	var $win = $(window),
-		$fv = $('.fv'),
-		$header = $('.header')
-	fvHeight = $fv.outerHeight(),
-		fixedClass = 'fixed';
-
-	$win.on('load scroll', function () {
+		$fv = $('.bl_MV'),
+		$header = $('.ly_header'),
+		fvHeight = $fv.outerHeight();
+		fixedClass = 'is_fixed';
+	
+	$win.on('load scroll', function(){
 		var value = $(this).scrollTop();
-		if ($win.width() > 768)
-
-			if (value > fvHeight) {
+		if($win.width() > 768){
+			//valueがスクロールした量
+			if(value > fvHeight) {
 				$header.addClass(fixedClass);
-
 			} else {
 				$header.removeClass(fixedClass);
-
 			}
+		}
 	});
 });
